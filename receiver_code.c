20300aa89725f1f3fa9ec476d36fd79e4c3bb3c5
smwq <smwaqarquadri@gmail.com>
2018-03-30 23:54:47 +0530

    Create receiver_code.c

---
+++ receiver_code.c
@@ -0,0 +1,31 @@
+#include <Servo.h>
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;
+void setup()
+{
+  Serial.begin(9600);
+  myservo1.attach(8);
+  myservo2.attach(9);
+  myservo3.attach(10);
+  myservo4.attach(11);
+  myservo5.attach(12);
+}
+void loop()
+{
+  if(Serial.available() >=5)
+  {
+    byte servoAng1 = Serial.read();
+    byte servoAng2 = Serial.read();
+    byte servoAng3 = Serial.read();
+    byte servoAng4 = Serial.read();
+    byte servoAng5 = Serial.read();
+    myservo1.write(servoAng1);
+    myservo2.write(servoAng2);
+    myservo3.write(servoAng3);
+    myservo4.write(servoAng4);
+    myservo5.write(servoAng5);
+  }
+}
